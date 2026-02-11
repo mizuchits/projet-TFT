2aa8ccc107ea7fec5504a887d1adb0343098576f
Emilie Geneste <mimigeneste@gmail.com>
2026-02-11 09:38:02 +0100

    [modif]form_register_style_footer

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,15 +4,33 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('password')
+           ->add('username', TextType::class,[
+                'label' => 'Nom Utilisateur : ',
+                'attr' => [
+                    'class' => 'inscription-label'
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe : ',
+                'constraints' => [
+                    new NotBlank(['message' => 'Le mot de passe est obligatoire']),
+                    new Length([
+                        'min' => 4,
+                        'minMessage' => 'Le mot de passe doit contenir au moins {{ limit }} caractères',
+                    ]),
+                ],
+            ])
         ;
     }
 
